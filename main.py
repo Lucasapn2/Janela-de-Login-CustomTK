6fbc47099d0c84dde3cff892e5fe8dd097918da4
Lucas Barbosa <129186705+Lucasapn2@users.noreply.github.com>
2023-07-16 20:07:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-# import tkinter
-#
-#
-# Janela = tkinter.Tk()
-# Janela.geometry("200x300")
-#
-#
-# def clique():
-#     print("fazer login")
-#
-#
-# texto = tkinter.Label(Janela, text="Fazer Login")
-# texto.pack(padx=10, pady=10)
-#
-# botao = tkinter.Button(Janela, text="Login", command=clique)
-# botao.pack(padx=10, pady=10)
-#
-#
-#
-#
-#
-#
-#
-# Janela.mainloop()
-
 import customtkinter
 
 customtkinter.set_appearance_mode("dark")
@@ -30,11 +5,12 @@ customtkinter.set_default_color_theme("dark-blue")
 
 
 Janela = customtkinter.CTk()
-
+#Tamanho da Janela 
 Janela.geometry("500x300")
-def clique():
 
-    print("clicando")
+#Função Click
+def clique():
+  print("clicando")
 
 texto = customtkinter.CTkLabel(Janela, text="fazer Login")
 texto.pack(padx=10, pady=10)
@@ -49,13 +25,8 @@ checkbox = customtkinter.CTkCheckBox(Janela, text="lembrar login")
 checkbox.pack(padx= 10 , pady = 10)
 
 
-
 botao = customtkinter.CTkButton(Janela, text="login", command=clique)
 botao.pack(padx=10, pady=10)
-
-
-
-
 
 Janela.mainloop()
 
